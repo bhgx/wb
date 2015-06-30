06ce1c695fabf87dbe9fa5fea4cdc5bb2ffa6383
钟望 <zhongwang@haizhi.com>
2015-06-30 19:40:17 +0800

    ui

--- js/script.js
+++ js/script.js
@@ -3,15 +3,23 @@ $(function(){
 	var $login = $('#login'),
 		$submit = $('#submit'),
 		$close = $ ('#close'),
-		$ceng = $('#ceng');
+		$ceng = $('#ceng'),
+		$step = $('#lc-step'),
+		$success = $('#lc-success');
+
 	$login.on('click',function(){
 		$ceng.fadeIn(400);
 	});
 
 
 	$submit.on('click', function(){
-
-		$ceng.fadeOut(600);
+		$step.css({
+			display: 'none'
+		});
+		$success.css({
+			display: 'block'
+		});
+		//$ceng.fadeOut(600);
 	});
 
 	$close.on('click', function(){
