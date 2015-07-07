8bb2cca49f9bf1f0d26419f0a04a630973c8a14d
钟望 <zhongwang@haizhi.com>
2015-07-07 13:24:41 +0800

    ui

--- js/script.js
+++ js/script.js
@@ -61,6 +61,7 @@ $(function(){
 
 	if (is_weixn()){
 	    goDownload.onclick=function(e){
+	    	alert('weixin')
 	        e.preventDefault();
 	        loadWrap.fadeIn(500);
 	        setTimeout(function(){
