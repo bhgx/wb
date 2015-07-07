0a32dc9694e05dfffcd9035f06416923e50ffc35
钟望 <zhongwang@haizhi.com>
2015-07-07 16:43:44 +0800

    ui

--- js/script.js
+++ js/script.js
@@ -69,7 +69,6 @@ $(function(){
 	        return false;
 	    }
 	} else {
-		goDownload.href="http://182.92.240.52/play/apk/paibao.apk?"+new Date().getTime();
 		if(isIOS){
 		    goDownload.href="";
 		} else if (isAndroid) {
